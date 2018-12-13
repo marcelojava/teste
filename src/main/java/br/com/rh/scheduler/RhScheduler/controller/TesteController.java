0f10f5a69b8e47be9245c6749dd13be465acaefb
Marcelo Silva <masilva@vr.com.br>
2018-12-13 10:33:04 -0200

    add log

--- src/main/java/br/com/rh/scheduler/RhScheduler/controller/TesteController.java
+++ src/main/java/br/com/rh/scheduler/RhScheduler/controller/TesteController.java
@@ -1,13 +1,17 @@
 package br.com.rh.scheduler.RhScheduler.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TesteController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(TesteController.class);
+
     @Scheduled(fixedDelay = 10000l)
     public void teste() {
-        System.out.println("teste");
+        LOG.info("teste");
     }
 }
